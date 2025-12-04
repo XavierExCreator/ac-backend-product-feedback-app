61efc6255e14c5cbf3824aa234dc50cd5d079584
XavierExCreator <xavier.ex.pro@gmail.com>
2025-12-04 12:12:26 -0500

    Removing and readding .gitignore to make sure it covers the files I need. Changing link to api to make sure it's secure.

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  databaseUrl: "postgresql://neondb_owner:npg_6tRIQs5vWqjo@ep-tiny-night-afmeo4on-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  databaseUrl: "postgresql://neondb_owner:npg_vgLS0ZfhW7et@ep-tiny-night-afmeo4on-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
